d5f68f63a0b42cb062d490144448f646e24a89ec
Mehtab009 <jagjeetjagjeet03@gmail.com>
2026-03-15 10:44:34 -0400

    Add files via upload

---
+++ Controllers/InsureeController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+using System.Web;
+using System.Web.Mvc;
+using CarInsurance.Models;
+
+namespace CarInsurance.Controllers
+{
+    public class InsureeController : Controller
+    {
+        private InsuranceEntities2 db = new InsuranceEntities2();
+
+        public ActionResult Index()
+        {
+            return View(db.Insurees.ToList());
+        }
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Insuree insuree = db.Insurees.Find(id);
+            if (insuree == null)
+            {
+                return HttpNotFound();
+            }
+            return View(insuree);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,FirstName,LastName,EmailAddress,DateOfBirth,CarYear,CarMake,CarModel,DUI,SpeedingTickets,CoverageType,Quote")] Insuree insuree)
+        {
+            if (ModelState.IsValid)
+            {
+                int baseQuote = 50;
+                int quote = baseQuote;
+
+                int age = DateTime.Now.Year - insuree.DateOfBirth.Value.Year;
+                if (age < 18) quote += 100;
+                else if (age >= 18 && age <= 25) quote += 50;
+                else quote += 25;
+
+                if (insuree.CarYear < 2000) quote += 25;
+                if (insuree.CarYear > 2015) quote += 25;
+
+                if (insuree.CarMake.ToLower() == "porsche")
+                {
+                    quote += 25;
+                    if (insuree.CarModel.ToLower() == "911 carrera") quote += 25;
+                }
+
+                quote +=((insuree.SpeedingTickets ?? 0) * 10);
+                if (insuree.DUI == true) quote = (int) (quote * 1.25);
+                if (insuree.CoverageType == "Full") quote = (int)(quote * 1.50);
+
+                insuree.Quote = quote;
+
+                db.Insurees.Add(insuree);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(insuree);
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null) return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            Insuree insuree = db.Insurees.Find(id);
+            if (insuree == null) return HttpNotFound();
+            return View(insuree);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,FirstName,LastName,EmailAddress,DateOfBirth,CarYear,CarMake,CarModel,DUI,SpeedingTickets,CoverageType,Quote")] Insuree insuree)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(insuree).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(insuree);
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null) return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            Insuree insuree = db.Insurees.Find(id);
+            if (insuree == null) return HttpNotFound();
+            return View(insuree);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Insuree insuree = db.Insurees.Find(id);
+            db.Insurees.Remove(insuree);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Admin()
+        {
+            return View(db.Insurees.ToList());
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing) db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
